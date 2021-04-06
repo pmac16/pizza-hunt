9dffe14b06a0fad5c59c018e41b351af69ec9aa6
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-05 22:04:53 -0700

    integrate with the front end

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model} = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
 //create the Pizza model using the PizzaSchema
 
 
@@ -11,14 +12,34 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
-});
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    toJson: {
+        virtuals: true, 
+        getters: true    
+    },
+    id: false
+}
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 const Pizza = model('Pizza', PizzaSchema);
 
